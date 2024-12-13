0b7ab584061ee3601eedbab9d1a6fedb73453cf7
jhon <jhonmarcosaraujo@yahoo.com>
2024-12-12 21:21:28 -0400

    routes and api partially finished

--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
 import express from "express";
 import { join, dirname } from "path";
 import { fileURLToPath } from "url";
-import indexRoutes from "./routes/routes.js"; // Archivo con las rutas de la aplicación
+import indexRoutes from "./routes/routes.js"; 
 
-// Obtiene la ruta actual del archivo
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const app = express();
 
-// Configuración de archivos estáticos (sirve archivos desde /public)
 app.use(express.static('public', {
     setHeaders: (res, path) => {
         if (path.endsWith('.js')) {
@@ -18,17 +17,18 @@ app.use(express.static('public', {
     }
 }));
 
-// Configuración del puerto
 app.set("port", process.env.PORT || 3000);
 
-// Configuración de la carpeta de vistas y motor de plantillas
-app.set("views", join(__dirname, "views")); // Ruta a la carpeta de vistas
-app.set("view engine", "ejs"); // Configura EJS como motor de plantillas
+app.set("views", join(__dirname, "views")); 
+app.set("view engine", "ejs"); 
 
-// Usa las rutas definidas en routes.js
+
+
 app.use(indexRoutes);
 
-// Inicia el servidor
+// Run server
+
+
 app.listen(app.get("port"), () => {
-  console.log(`Servidor corriendo en http://localhost:${app.get("port")}`);
+  console.log(`Server running on =>  http://localhost:${app.get("port")}`);
 });
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -4,15 +4,19 @@ import { Router } from "express";
 
 const router = Router();
 
-
 router.get("/",(req,res)=> {
 
     res.render("index",{country:"DOM"})
     
-    
 });
 
+router.get("/windy",(req,res)=>{
+    res.redirect("https://www.windy.com/?19.204,-69.752,8,m:ecraedK")
+})
 
+router.get("/:another",(req,res)=>{
+    res.redirect("/");
+})
 
 export default router;
 
